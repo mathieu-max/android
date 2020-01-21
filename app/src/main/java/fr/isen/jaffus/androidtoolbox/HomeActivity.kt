836c95353c68c22fd6ab94004139db72b6bfeaf6
mathieu.jaffus <mathieu.jaffus@isen.yncrea.fr>
2020-01-21 16:53:22 +0100

    Fin partie 2

--- app/src/main/java/fr/isen/jaffus/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/jaffus/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,24 @@
 package fr.isen.jaffus.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        idButtonCycle.setOnClickListener {
+            startActivity( Intent (this@HomeActivity, LifeCycleActivity::class.java))
+        }
+
+        idButtonDeco.setOnClickListener {
+            setContentView(R.layout.activity_login)
+            finish()
+        }
     }
 }
